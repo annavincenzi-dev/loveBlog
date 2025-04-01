2ea8e455abf258d7a6faa029fffc48362524c212
Anna Vincenzi <vincenzi.anna.41@gmail.com>
2025-04-01 16:16:47 +0200

    correzione di un bug in fase di upload di una storia

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Story;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Routing\Controllers\HasMiddleware;
 
--- database/migrations/2025_03_29_130642_add_user_id_to_stories_table.php
+++ database/migrations/2025_03_29_130642_add_user_id_to_stories_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::table('stories', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id')->nullable()->after('title');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->dropCOlumn('written_by');
         });
         
     }
@@ -25,9 +24,13 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('stories', function (Blueprint $table) {
+            
             $table->dropForeign(['user_id']);
+
             $table->dropColumn('user_id');
-            $table->string('written_by')->after('title')->nullable();
+
+            
+            
         });
     }
 };
